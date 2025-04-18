88bbb5a3d45b8419b1c232ac7790cbe2233b5c7f
loulou4188 <129610631+loulou4188@users.noreply.github.com>
2025-04-18 16:02:18 +0200

    Update export.c

--- build-in/export.c
+++ build-in/export.c
@@ -16,7 +16,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-t_env *ft_export(t_env *env, const char *key, const char *value)
+t_env *export(t_env *env, const char *key, const char *value)
 {
     t_env *current;
     t_env *new_node;
@@ -28,13 +28,15 @@ t_env *ft_export(t_env *env, const char *key, const char *value)
         if (ft_strcmp(current->key, key) == 0)
         {
             free(current->value);
-            current->value = strdup(value);
+            current->value = ft_strdup(value);
             return (env);
         }
         current = current->next;
     }
     //不然新造一个
     new_node = (t_env *)malloc(sizeof(t_env));
+    if (!new_node)
+        return (env);
     new_node->key = ft_strdup(key);
     new_node->value = ft_strdup(value);
     new_node->next = env;
