38e1348524a4fd5215ae2cc0be96475353846004
loulou4188 <129610631+loulou4188@users.noreply.github.com>
2025-04-18 16:24:58 +0200

    Add files via upload

---
+++ execute_builtin.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execute_builtin.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: loulou <loulou@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/18 15:59:47 by loulou            #+#    #+#             */
+/*   Updated: 2025/04/18 16:24:08 by loulou           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int excute_builtin(t_shell *shell, char **args)
+{
+    char *equal_sign;
+    if (!args[0])
+        return (1);
+    if (ft_strcmp(args[0], "exit") == 0)
+        ft_exit(args);
+    else if (ft_strcmp(args[0], "pwd") == 0)
+        ft_pwd();
+    else if (ft_strcmp(args[0], "echo") == 0)
+        ft_echo(args);
+    else if (ft_strcmp(args[0], "cd") == 0)
+        return (ft_cd(args, shell->envlist));
+    else if (ft_strcmp(args[0], "env") == 0)
+        ft_env(shell->envlist);
+    else if (ft_strcmp(args[0], "unset") == 0)
+        shell->envlist = ft_unset(shell->envlist, args[1]);
+    else if (ft_strcmp(args[0], "export") == 0)
+    {
+        if (args[1])
+        {
+            equal_sign = ft_strchr(args[1], '=');
+            if (equal_sign)
+            {
+                *equal_sign = '\0';
+                shell->envlist = ft_export(shell->envlist, args[1], (equal_sign + 1));
+                *equal_sign = '=';
+            }
+            else
+                error("minishell: export: invalid format (expected VAR=VALUE)\n");
+        }
+        else
+            ft_env(shell->envlist);
+    }
+    return (0);
+}
