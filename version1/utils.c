f4c9e5ce63b3fe161296af59ec8d0c8f9e6b8411
Aliceparis <130312924+Aliceparis@users.noreply.github.com>
2025-06-16 11:02:28 +0200

    Delete version1 directory

--- version1/utils.c
+++
@@ -1,8 +0,0 @@
-#include "minishell.h"
-
-void error_message(t_shell *shell_program, const char *msg, int exit_s)
-{
-    ft_fprintf(stderr, "%s\n",msg);
-    shell_program->exit_status = exit_s;
-}
-
